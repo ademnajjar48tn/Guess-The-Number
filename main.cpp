17e40d5962dd91628c1193df812854901d985cb9
ademnajjar48tn <cctestingportugal@gmail.com>
2024-05-26 22:30:21 +0100

    Created the main method🛸

--- main.cpp
+++ main.cpp
@@ -1,1 +1,19 @@
 #include <iostream>
+
+int main() {
+	
+	srand(time(NULL));
+	int randomNum = (std::rand() % 100) + 1;
+	std::cout << "Guess a the random number bettween(1, 100) 🎲"<<'\n';
+	int guess;
+	int tries = 0;
+	do {
+		std::cout << "Enter number: ";
+		std::cin >> guess;
+		tries++;
+	}
+	while(guess!= randomNum);
+	std::cout << "You guessed the number in " << tries << " tries 🏆";
+	
+	return 0;
+}
